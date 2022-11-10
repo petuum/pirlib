6e1f48880fcb7232a4f45406ffbc5af92c0acfd7
Nilabhra <nilabhraroy@gmail.com>
2022-11-10 10:47:43 +0400

    feat: setting the env vars before generation of the argo yaml.

--- example/run_argo.sh
+++ example/run_argo.sh
@@ -4,15 +4,18 @@ ROOTDIR=$EXAMPLEDIR/..
 ### Module_1: Docker_Packaging
 python $ROOTDIR/bin/pircli dockerize \
     --auto $ROOTDIR \
-	--pipeline example.example:infer_pipeline \
 	--pipeline example.example:train_pipeline \
 	--output $EXAMPLEDIR/package_argo.yml \
 	--flatten
 
 ### Module_2: Argoize_Module
+INPUT_train_dataset=$EXAMPLEDIR/inputs/train_dataset \
+INPUT_translate_model=$EXAMPLEDIR/inputs/translate_model.txt \
+INPUT_sentences=$EXAMPLEDIR/inputs/sentences \
+OUTPUT=$EXAMPLEDIR/outputs \
 python  $ROOTDIR/bin/pircli argoize $EXAMPLEDIR/package_argo.yml \
 	--target pirlib.backends.argo_batch:ArgoBatchBackend \
-	--output $EXAMPLEDIR/argo-compose.yml
+	--output $EXAMPLEDIR/argo-train.yml
 
 ### Module_3: Argo_Execute_Module
 # PYTHONPATH=$ROOTDIR $ROOTDIR/bin/pircli execute_argo $EXAMPLEDIR/argo-compose.yml \
