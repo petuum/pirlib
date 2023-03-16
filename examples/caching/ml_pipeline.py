feaaa6a5774ec12af1bfc6c0290f5af7ec53b681
Nilabhra <nilabhraroy@gmail.com>
2023-03-16 12:11:43 +0400

    feat: modified example.

--- examples/caching/ml_pipeline.py
+++ examples/caching/ml_pipeline.py
@@ -4,16 +4,8 @@ from pirlib.iotypes import DirectoryPath, FilePath
 from pirlib.pipeline import pipeline
 from pirlib.task import task
 
-# def generate_preprocess_key():
-#     # Create the parameters for the preprocessing routine.
-#     hparams = [1, 2, 3, 4.2, 5]
-#     sampled_hparams = random.choice(hparams)
-#     # Compute a hash value from the parameters to be used by task wrapper.
-#     preprocess_cache_key = hashlib.sha256(f"preprocess_{sampled_hparams}".encode()).hexdigest()
-#     return preprocess_cache_key
 
-
-@task(config=dict(cache=True, cache_key_file="hparams"))
+@task(cache=True, cache_key_file="hparams")
 def preprocess(dataset: DirectoryPath, *, hparams: FilePath) -> DirectoryPath:
     # Read the raw data.
     with (dataset / "data.txt").open("r") as f:
@@ -31,17 +23,7 @@ def preprocess(dataset: DirectoryPath, *, hparams: FilePath) -> DirectoryPath:
     return output_dir
 
 
-# def generate_train_key():
-#     # Create the parameters for the train routine.
-#     hparams = [199, 2.12, 12313, 4.2, 123, 5]
-#     sampled_hparams = random.choice(hparams)
-
-#     # Compute a hash value from the parameters to be used by task wrapper.
-#     train_cache_key = hashlib.sha256(f"train_{sampled_hparams}".encode()).hexdigest()
-#     return train_cache_key
-
-
-@task(config=dict(cache=True, cache_key_file="hparams"))
+@task(cache=True, cache_key_file="hparams")
 def train(preprocessed_dir: DirectoryPath, *, hparams: FilePath) -> DirectoryPath:
     # Read the preprocessed data.
     with (preprocessed_dir / "preprocessed_data.txt").open("r") as f:
@@ -59,17 +41,7 @@ def train(preprocessed_dir: DirectoryPath, *, hparams: FilePath) -> DirectoryPat
     return output_dir
 
 
-# def generate_key():
-#     # Create the parameters for the postprocess routine.
-#     hparams = [13, 0.2, 123, 123, 14]
-#     sampled_hparams = random.choice(hparams)
-
-#     # Compute a hash value from the parameters to be used by task wrapper.
-#     postprocess_cache_key = hashlib.sha256(f"postprocess_{sampled_hparams}".encode()).hexdigest()
-#     return postprocess_cache_key
-
-
-@task(config=dict(cache=True, cache_key_file="hparams"))
+@task(cache=True, cache_key_file="hparams")
 def postprocess(
     preprocessed_dir: DirectoryPath, model_dir: DirectoryPath, *, hparams: FilePath
 ) -> DirectoryPath:
--- examples/caching/run_argo.sh
+++ examples/caching/run_argo.sh
@@ -9,20 +9,20 @@ python $ROOTDIR/bin/pircli dockerize \
 	--output $EXAMPLEDIR/package_argo.yml \
 	--flatten
 
-# Convert EXAMPLEDIR to absolute path since docker can't bind-mount relative paths.
-EXAMPLEDIR=$([[ $EXAMPLEDIR = /* ]] && echo "$EXAMPLEDIR" || echo "$PWD/${EXAMPLEDIR#./}")
+# # Convert EXAMPLEDIR to absolute path since docker can't bind-mount relative paths.
+# EXAMPLEDIR=$([[ $EXAMPLEDIR = /* ]] && echo "$EXAMPLEDIR" || echo "$PWD/${EXAMPLEDIR#./}")
 
-### Module 2: Argoize_Module
-INPUT_raw_data=$EXAMPLEDIR/dataset \
-INPUT_preproc_hp=$EXAMPLEDIR/dataset/preproc_hp.txt \
-INPUT_train_hp=$EXAMPLEDIR/dataset/train_hp.txt \
-INPUT_postproc_hp=$EXAMPLEDIR/dataset/postproc_hp.txt \
-CACHE=$EXAMPLEDIR/cache_dir \
-OUTPUT=$EXAMPLEDIR/outputs \
-NFS_SERVER=k8s-master.cm.cluster \
-python  $ROOTDIR/bin/pircli generate $EXAMPLEDIR/package_argo.yml \
-	--target pirlib.backends.argo_batch:ArgoBatchBackend \
-	--output $EXAMPLEDIR/argo-ml-pipeline.yml
+# ### Module 2: Argoize_Module
+# INPUT_raw_data=$EXAMPLEDIR/dataset \
+# INPUT_preproc_hp=$EXAMPLEDIR/dataset/preproc_hp.txt \
+# INPUT_train_hp=$EXAMPLEDIR/dataset/train_hp.txt \
+# INPUT_postproc_hp=$EXAMPLEDIR/dataset/postproc_hp.txt \
+# CACHE=$EXAMPLEDIR/cache_dir \
+# OUTPUT=$EXAMPLEDIR/outputs \
+# NFS_SERVER=k8s-master.cm.cluster \
+# python  $ROOTDIR/bin/pircli generate $EXAMPLEDIR/package_argo.yml \
+# 	--target pirlib.backends.argo_batch:ArgoBatchBackend \
+# 	--output $EXAMPLEDIR/argo-ml-pipeline.yml
 
 # Run the Argo workflow
 #argo submit -n argo --watch $EXAMPLEDIR/argo-ml-pipeline.yml
